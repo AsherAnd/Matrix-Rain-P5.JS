cf4e5fb6537d14d3a0cd0eb9379f3a28422c39db
AsherAnd <ashertew@gmail.com>
2022-06-21 00:45:14 -0400

    Added Matrix Rain code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Matrix Rain Code in P5.JS
+
+This is the p5.JS version of my previous Matrix project made in python.
+
+The program displays Katakana characters falling down the screen same as the intro to the Matrix movie with some key differences here and there.
+
+You can see a live demo here at https://asherand.github.io/Matrix-Rain-p5.JS/
+
+![alt text](https://raw.githubusercontent.com/AsherAnd/Matrix-Rain-p5.JS/main/preview.png)
---
+++ script.js
@@ -0,0 +1,110 @@
+//////////////////////////////////////////////////////////////
+//                                                            //
+// Created by: Asher Andargachew                              //
+// Created on: July 31st, 2021                                //
+// Description: Matrix Rain Code from the Matrix movie intro  //
+//              in Javascript.                                //
+//                                                            //
+////////////////////////////////////////////////////////////////
+
+let bgColor;
+const space = 9;
+var character = [];
+
+// when screen size changes
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight * 0.9);
+
+  // reset values
+  clear();
+  character = [];
+
+  setup();
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  console.log(`Width: ${width}, Height ${height}`);
+
+  colorMode(HSB, 360, 100, 100);
+  bgColor = color(0);
+  background(bgColor);
+
+  // create new instance of katakana class
+  character = new Array(width);
+
+  for (i = 0; i <= character.length; i++) {
+    if (i % space == 0) {
+      character[i] = new katakana(i);
+    }
+  }
+}
+
+function draw() {
+  background(bgColor);
+
+  // The matrix character(s)
+  for (i = 0; i <= character.length; i++) {
+    if (i % space == 0) {
+      character[i].rain();
+    }
+  }
+}
+
+class katakana {
+  constructor(x) {
+    // properties of the object
+    this.x = x;
+    this.y = random(-1000, -800);
+    this.fallSpeed = random(0.003 * height, 0.01 * height);
+    this.value;
+    this.switchSpeed = round(random(2, 30));
+    this.textSize = 14;
+    this.stream = new Array(round(random(5, 35)));
+    this.color = "Green"; // Green or Red
+  }
+
+  rain() {
+    // The uni-code of katakana characters
+    for (let j = 0; j < this.stream.length; j++) {
+      if (frameCount % this.switchSpeed == 0) {
+        this.value = String.fromCharCode(0x30a0 + round(random(0, 96)));
+        this.stream[j] = this.value;
+      }
+    }
+
+    this.show();
+  }
+
+  show() {
+    for (let j = 0; j < this.stream.length; j++) {
+      // Change color from top to bottom
+      if (j == this.stream.length - 1) {
+        if (this.color == "Red") {
+          fill(0, 60, 100);
+        } else {
+          fill(127, 30, 100);
+        }
+      } else {
+        if (this.color == "Red") {
+          fill(0, 100, map(j, 0, this.stream.length - 1, 0, 100));
+        } else {
+          fill(127, 100, map(j, 0, this.stream.length - 1, 0, 70));
+        }
+      }
+      text(`${this.stream[j]}`, this.x, this.y - j * -this.textSize);
+    }
+    this.update();
+    this.update();
+  }
+
+  update() {
+    // Increase the fall of the character(s)
+    this.y += this.fallSpeed;
+
+    // When the character(s) falls past the screen
+    if (this.y > height) {
+      this.y = -height / 2;
+    }
+  }
+}
